91a086297d5862f0bf61e14d1b4efb89044909fd
Abhishek Uniyal <abhiuni02@gmail.com>
2026-02-03 08:24:19 +0530

    Add calculator functionality in practice.cs

---
+++ scripts/practice.cs
@@ -0,0 +1,61 @@
+using System;
+
+public class HelloWorld
+{
+    public static int Calculator( int choice ,int a ,int b)
+    {
+        switch (choice)
+        {
+            case 1 :
+                return a+b;
+                break;
+            case 2:
+                return a-b;
+                break;
+            case 3:
+                return a*b;
+                break;
+            case 4:
+                return a/b;
+                break;
+            default:
+                return 1;
+                break;
+                
+            
+        }
+    }
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Choose:-\n1-ADD \n2-Subtract \n3-Multiply \n4-Divide");
+     
+      int choice = int.Parse(Console.ReadLine());
+      Console.WriteLine("Enter first number : ");
+      int a = int.Parse(Console.ReadLine());
+      Console.WriteLine("Enter Second number : ");
+      int b = int.Parse(Console.ReadLine());
+      int result=  Calculator(choice , a ,b);
+         
+        if (choice ==1)
+        {
+            Console.WriteLine("Sum of Two number is " + result);
+        }
+        else if ( choice ==2)
+        {
+             Console.WriteLine("Subtraction of Two number is " + result);
+        }
+         else if ( choice ==3)
+        {
+             Console.WriteLine("Multiplication of Two number is " + result);
+        }
+         else if ( choice ==4)
+        {
+             Console.WriteLine("Division of Two number is " + result);
+        }
+        else 
+        {
+            Console.WriteLine("Please Choose a correct Option.");
+        }
+      
+    }
+}
